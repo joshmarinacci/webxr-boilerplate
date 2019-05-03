87352560a4facb8b225104df6fc960294075f8ce
joshmarinacci <joshua@marinacci.org>
2019-05-02 21:23:41 -0700

    start making an index.js

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-export const foo = "bar"
-
-import {VRStats} from "./v2/vrstats.js"
+import VRStats from "./v2/vrstats.js"
 import VRManager, { VR_DETECTED} from "./v2/vrmanager.js"
 import {POINTER_CLICK, POINTER_ENTER, POINTER_EXIT, POINTER_MOVE, POINTER_PRESS, POINTER_RELEASE, Pointer} from "./v2/Pointer.js"
 export {
